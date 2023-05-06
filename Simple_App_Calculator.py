0457ce7dda4de4187f531fe51b4f68faa6f3986a
vrglhuhu <cheanbvergel@gmail.com>
2023-05-07 01:12:59 +0800

    My Psuedocodes.

--- Simple_App_Calculator.py
+++ Simple_App_Calculator.py
@@ -1,3 +1,30 @@
 # Vergel, Chean Bernard Villanueva
-# Assignment No. 5
 # Simple App Calculator
+
+# Print selecting operation
+    # Ask for the operation that the user wants 
+    # Check if the choice is one among the four operations
+        # Calculate the given operation.
+        # If the user wants addition
+                # Ask the user to enter first number  
+                # Ask the user to enter second number    
+                # Printing result    
+            # If the user input other character  
+        # If the user wants subtraction    
+                # Ask the user to enter first number   
+                # Ask the user to enter second number    
+                # Printing result   
+            # If the user input other character
+        # If the user wants multiplication
+                # Ask the user to enter first number
+                # Ask the user to enter second number
+                # Printing result
+            # If the user input other character
+        # If the user wants division       
+                # Ask the user to enter first number
+                # Ask the user to enter second number
+                # Printing result
+            # If the user input other character or zero as the second number
+        # Ask user if they want to have another calculation
+        # Break the while loop if user answered no
+    # If not in the four operations   
