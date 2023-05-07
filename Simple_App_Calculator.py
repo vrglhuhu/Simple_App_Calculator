f8b75ae27065228356c310091ac41596ceeff391
vrglhuhu <cheanbvergel@gmail.com>
2023-05-07 08:33:01 +0800

    If the user wants addition.

--- Simple_App_Calculator.py
+++ Simple_App_Calculator.py
@@ -14,13 +14,23 @@ while True:
 
     # Check if the choice is one among the four operations
     if choice.lower() in ('a', 'b', 'c', 'd'):
-        
+
         # Calculate the given operation.
         # If the user wants addition
-                # Ask the user to enter first number  
-                # Ask the user to enter second number    
-                # Printing result    
-            # If the user input other character  
+        if choice.lower() == 'a':
+            print("ADDITION OPERATION")
+            try:
+                # Ask the user to enter first number
+                num1 = float(input("Enter first number that you want to add: "))
+                # Ask the user to enter second number
+                num2 = float(input("Enter second number that you want to add: "))
+                add = num1 + num2
+                # Printing result
+                print(num1, "+", num2, "=", add)
+            # If the user input other character
+            except ValueError:
+                print("INVALID INPUT. Please enter a number.")
+                continue   
         # If the user wants subtraction    
                 # Ask the user to enter first number   
                 # Ask the user to enter second number    
